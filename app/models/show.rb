6d138d1a4b366b7a641be6ee02017de87b7f2f19
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-02 16:43:12 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -22,10 +22,6 @@ class Show < ActiveRecord::Base
   end
 end
 
-# least_popular_show: returns the show with the lowest rating.
-#
-# ratings_sum: returns the sum of all of the ratings.
-#
 # popular_shows: returns an array of all of the shows that have a rating greater than 5. hint: use the where Active Record method.
 #
 # shows_by_alphabetical_order: returns an array of all of the shows sorted by alphabetical order according to their names. hint: use the order Active Record method.
