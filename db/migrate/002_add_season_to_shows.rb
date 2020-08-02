47591e0c3752f334f3faee436d3413a9c5643a06
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-02 16:31:46 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,4 +1,4 @@
-class AddPartNumberToProducts < ActiveRecord::Migration[5.0]
+class AddSeasonToShows < ActiveRecord::Migration[5.0]
   def change
     add_column :products, :part_number, :string
   end
